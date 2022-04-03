a49b80acfc95a413918f5505b68d4b046700d425
Xalexxx284 <94836307+Xalexxx284@users.noreply.github.com>
2022-04-04 00:56:25 +0200

    Create PAC5_Triggers.sql

---
+++ UF3 - BBDD/PACs/PAC5_Triggers.sql
@@ -0,0 +1,4 @@
+-- Ej1 -----------------------------------------------------------------------------------------------------------------
+/* Programar un trigger esmentat nom_departament_notnull que s’activarà quan no s’especifiqui el nom del departament al
+donar d’alta un departament nou a la taula departments. */
+-- ---------------------------------------------------------------------------------------------------------------------
